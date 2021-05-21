341a09ce99bafb3c123416dcb8d5675090c2ca04
Melonnie <melonnie13@gmail.com>
2021-05-20 22:35:22 -0500

    finished backend setup, started front end set up

--- backend/Routes/api/index.js
+++ backend/Routes/api/index.js
@@ -8,30 +8,18 @@ const usersRouter = require('./users.js');
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
-
 //^^ connects all the routes exported from the session and users files
 
-
-
-module.exports = router;
-
-
-
-
-
-
-
-
-// router.post('/test', function(req, res) {
-//     res.json({ requestBody: req.body });
-//   });
-//   //^^ A router is created and
-//   // and API test route is added to the router.
-//   // the API test route is accepting reqs
-//   // with the URL path of /api/test with the
-//   // HTTP verb of POST.
-//   // It sends a JSON res containing
-//   // whatever is in the body of the req.
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+});
+//^^ A router is created and
+// and API test route is added to the router.
+// the API test route is accepting reqs
+// with the URL path of /api/test with the
+// HTTP verb of POST.
+// It sends a JSON res containing
+// whatever is in the body of the req.
 
 
 // The main purpose of this express app is to be a REST API server.
@@ -45,35 +33,38 @@ module.exports = router;
 // const { setTokenCookie } = require('../../utils/auth.js');
 // const { User } = require('../../db/models');
 // router.get('/set-token-cookie', asyncHandler(async (req, res) => {
-//   const user = await User.findOne({
-//       where: {
-//         username: 'Demo-lition'
-//       },
-//     })
-//   setTokenCookie(res, user);
-//   return res.json({ user });
-// }));
+    //   const user = await User.findOne({
+        //       where: {
+            //         username: 'Demo-lition'
+            //       },
+            //     })
+            //   setTokenCookie(res, user);
+            //   return res.json({ user });
+            // }));
 
 
-// // GET /api/restore-user
-// const { restoreUser } = require('../../utils/auth.js');
-// router.get(
-//   '/restore-user',
-//   restoreUser,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-//^^ tests the restoreUser middleware by
-// connecting the middleware and checking whether
-// or not the req.user key has been populated by the middleware properly.
+            // // GET /api/restore-user
+            // const { restoreUser } = require('../../utils/auth.js');
+            // router.get(
+                //   '/restore-user',
+                //   restoreUser,
+                //   (req, res) => {
+                    //     return res.json(req.user);
+                    //   }
+                    // );
+                    //^^ tests the restoreUser middleware by
+                    // connecting the middleware and checking whether
+                    // or not the req.user key has been populated by the middleware properly.
 
-// const { requireAuth } = require('../../utils/auth.js');
-// router.get(
-//   '/require-auth',
-//   requireAuth,
-//   (req, res) => {
-//     return res.json(req.user);
-//   }
-// );
-// ^^ test for requireAuth - if no session user the route will return an "Unauthorized" error
+                    // const { requireAuth } = require('../../utils/auth.js');
+                    // router.get(
+                        //   '/require-auth',
+                        //   requireAuth,
+                        //   (req, res) => {
+                            //     return res.json(req.user);
+                            //   }
+                            // );
+                            // ^^ test for requireAuth - if no session user the route will return an "Unauthorized" error
+
+
+module.exports = router;
--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -14,14 +14,14 @@ router.use('/api', apiRouter);
 // ^^ all of the URLs of the routes in the api router
 //    will be prefixed with /api
 
-router.get('/hello/world', function(req, res) {
-  //^^ be sure to pay attention to the path;
-  // remember I was at localhost5000/ and received
-  // a 404 backend and a cannot GET / browser error
-  // because I NEEDED to be at localhost5000/hello/world
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World');
-});
+// router.get('/hello/world', function(req, res) {
+//   //^^ be sure to pay attention to the path;
+//   // remember I was at localhost5000/ and received
+//   // a 404 backend and a cannot GET / browser error
+//   // because I NEEDED to be at localhost5000/hello/world
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World');
+// });
 //^^ I am setting a cookie on the response with
 // the name of XSRF-TOKEN to the value of
 // the req.csrfToken method's return
