379ce7a782e246608b8511334f52b56921f8f929
Melonnie <melonnie13@gmail.com>
2021-05-20 20:44:45 -0500

    finished the signup route testing

--- backend/Routes/api/users.js
+++ backend/Routes/api/users.js
@@ -3,6 +3,35 @@
 // begin with /api/users
 
 const express = require('express')
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+//^^ creates an Express router
+
+// Sign up
+router.post(
+    //^^ POST/api/users route
+    '/',
+    asyncHandler(async (req, res) => {
+        //using the asyncHandler fn
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+      // ^^ call the signup static method on the User model
+
+      await setTokenCookie(res, user);
+      // if the user is successfully created, then call setTokenCookie
+
+      return res.json({
+          //^^ return a JSOn response with the user information
+        user,
+      });
+    }),
+    // if the creation of a user is unsuccessful then
+    // a Sequelize Validation error will be passed onto the
+    // next error-handling middleware
+  );
 
 module.exports = router;
